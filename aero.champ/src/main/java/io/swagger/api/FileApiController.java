8f263b08dce570c9aaa3ec9364315a87d2c36e78
KevinDAddison <kevindavid.addison@champ.aero>
2018-08-06 10:37:54 +0000

    Generated code by SwaggerHub

---
+++ aero.champ/src/main/java/io/swagger/api/FileApiController.java
@@ -0,0 +1,63 @@
+package io.swagger.api;
+
+import org.springframework.core.io.Resource;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class FileApiController implements FileApi {
+
+    private static final Logger log = LoggerFactory.getLogger(FileApiController.class);
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public FileApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    public ResponseEntity<Void> deleteFile(@ApiParam(value = "full path where the file will be retrieved",required=true) @PathVariable("file") String file,@ApiParam(value = "extension of the file",required=true) @PathVariable("ext") String ext) {
+        String accept = request.getHeader("Accept");
+        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<Resource> getFile(@ApiParam(value = "full path where the file will be retrieved",required=true) @PathVariable("file") String file,@ApiParam(value = "extension of the file",required=true) @PathVariable("ext") String ext,@ApiParam(value = "scale to render image") @Valid @RequestParam(value = "scaleSize", required = false) Integer scaleSize) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<Resource>(objectMapper.readValue("\"\"", Resource.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<Resource>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<Resource>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<Void> uploadFile(@ApiParam(value = "file detail") @Valid @RequestPart("file") MultipartFile file) {
+        String accept = request.getHeader("Accept");
+        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+}
